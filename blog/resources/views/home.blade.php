8bbc6a01454ee4c4c7d87fa4922c3b95be3f272b
Adam Hemming <hemming.dev@gmail.com>
2023-02-14 21:09:33 +0900

    create routs and basic main layout

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title', 'Blog - Home')
+
+@section('content')
+<h1>Home Page</h1>
+<p>This is the home page.</p>
+@endsection
---
+++ blog/resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Blog')</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="{{ route('home') }}">Home</a></li>
+      <li><a href="{{ route('about') }}">About</a></li>
+    </ul>
+
+    <div class="main">
+    @yield('content')
+    </div>
+  </body>
+</html>
